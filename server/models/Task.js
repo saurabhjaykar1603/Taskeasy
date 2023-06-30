049b682086fe302df0020d7930460fdf16090c31
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-30 16:55:43 +0530

    docs - mongoose schema defines

---
+++ server/models/Task.js
@@ -0,0 +1,9 @@
+import { Schema, model } from "mongoose";
+
+const taskSchema = new Schema({
+    tital: String,
+    description: String,
+})
+
+const Task = model('Task',taskSchema )
+export default Task
