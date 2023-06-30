1699e3da2298afcf4ceee8af870b9fc9b85a0f7f
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-30 16:45:52 +0530

    docs - database connected successfully

--- server/index.js
+++ server/index.js
@@ -6,9 +6,27 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// mongodb connection
+
+async function connectMongoDB() {
+    const conn = await mongoose.connect(process.env.MONGODB_URL)
+    if (conn) {
+        console.log("Connected to mongoDB")
+    }
+}
+connectMongoDB();
+
+app.get('/health',(req,res)=>{
+    res.json({
+        success:true,
+        message:'All good'
+    })
+})
+
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000,()=>{
-    console.log('listening on port'+ PORT)
+app.listen(5000, () => {
+    console.log('listening on port ' + PORT)
 })
 
