dd5915a90119862ffac5198f05bf601176a6feda
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-05 00:24:38 +0530

    feature - all task fetch successfully using get task api and we create a card for tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
@@ -7,40 +7,75 @@ function App() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
+  // get all tasks api 
+  const [tasks, setTasks] = useState([]);
+
+  const loadTask = async () => {
+    const { data } = await axios.get("/tasks")
+    setTasks(data?.data)
+  }
+
+  useEffect(()=>{
+    loadTask();
+
+  },[])
+
+
   // add task button api 
-  const addTask = async()=>{
-    const {data} = await axios.post('/task', {
-      title:title,
-      description:description
+  const addTask = async () => {
+    const { data } = await axios.post('/task', {
+      title: title,
+      description: description
     })
     alert(data?.message);
     setTitle("");
     setDescription("");
+    loadTask();
   }
   return (
     <>
       <div>
-        <h1 className='text-center mb-3 mt-4 taskeasy-title'>Task Easy 📝</h1>
+        <h1 className='text-center mb-4 mt-4 taskeasy-title'>Task Easy 📝</h1>
       </div>
 
-      <div className="row align-items-md-stretch m-auto">
+      <div className="row align-items-md-stretch m-auto ">
         <div className="col-md-6 mb-4">
-          <div className=" p-5 bg-warning  border rounded-3 shadow todo-container ">
+          <div className=" p-5 bg-warning  border rounded-3 shadow todo-container m-auto">
+          <h1 className='mb-3 text-center text-dark'>All Task</h1>
 
+            {
+              tasks.map((task) => {
+                return (
+                  <div className='task-card'>
+                    <div class="d-flex align-items-center px-3 py-2 my-4 text-white bg-white rounded shadow">
+                      {/* <img class="me-3" src="/docs/5.3/assets/brand/bootstrap-logo-white.svg" alt="" width="48" height="38"> */}
+                      <div class="lh-0 ">
+                        <h3 class=" mb-3 text-black lh-1 ">{task?.title}</h3>
+                        <p className='text-black mb-0'>{task?.description}</p>
+                      </div>
+                    </div>
+
+
+
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
         <div className="col-md-6 mb-4 ">
-          <div className="  p-5 py-5 pt-5  border rounded-3 shadow todo-form-container ">
-            <div className="task-input-btn-container d-grid  mt-3">
-              <input type="text" className="form-control"  placeholder='Enter Your Task Here' value={title} 
-              onChange={(e)=>{
-                setTitle(e.target.value);
-              }} />
-              <input type="text" className="form-control mt-3"  placeholder='Enter Your Description Here' value={description}
-              onChange={(e)=>{
-                setDescription(e.target.value);
-              }} />
-              <button className="btn btn-warning mt-3" type="button" onClick={addTask}>Add Task</button>
+          <div className="  p-5   border rounded-3 shadow todo-form-container ">
+            <h1 className='mb-4 text-center text-white'>Add Task</h1>
+            <div className="task-input-btn-container d-grid  ">
+              <input type="text" className="form-control" placeholder='Enter Your Task Here' value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }} />
+              <input type="text" className="form-control mt-4" placeholder='Enter Your Description Here' value={description}
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }} />
+              <button className="btn btn-warning mt-4" type="button" onClick={addTask}>Add Task</button>
             </div>
           </div>
         </div>
