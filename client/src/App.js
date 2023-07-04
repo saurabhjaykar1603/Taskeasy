27d48f7f69294d822e2cabc004a21f3998e008dc
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-04 09:55:48 +0530

    docs - values are asigned to usestate variable title and description

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './App.css'
 
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   return (
     <>
       <div>
@@ -18,9 +20,15 @@ function App() {
         <div className="col-md-6 mb-4 ">
           <div className="  p-5 py-5 pt-5  border rounded-3 shadow todo-form-container ">
             <div className="task-input-btn-container d-grid  mt-3">
-              <input type="text" className="form-control"  placeholder='Enter Your Task Here' />
-              <input type="text" className="form-control mt-3"  placeholder='Enter Your Description Here' />
-              <button className="btn btn-warning mt-3" type="button">Button</button>
+              <input type="text" className="form-control"  placeholder='Enter Your Task Here' value={title} 
+              onChange={(e)=>{
+                setTitle(e.target.value);
+              }} />
+              <input type="text" className="form-control mt-3"  placeholder='Enter Your Description Here' value={description}
+              onChange={(e)=>{
+                setDescription(e.target.value);
+              }} />
+              <button className="btn btn-warning mt-3" type="button">Add Task</button>
             </div>
           </div>
         </div>
