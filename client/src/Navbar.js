942dcb319a1094f80381465c82bb99e0572a6f56
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-27 16:58:48 +0530

    docs- navbar modified

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -4,20 +4,20 @@ function Navbar() {
   return (
     <div>
        <nav className="navbar navbar-expand-lg  navbar-con bg-white sticky-top">
-        <div className="container-fluid text-center p-2 ">
-            <a className="navbar-brand ms-2" href="./index.html">To DO Master</a>
+        <div className="container-fluid text-center p-2">
+            <a className="navbar-brand ms-5 fs-4" href="./index.html">To DO Master</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-auto ms-5">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        <a className="nav-link active fs-5" aria-current="page" href="/">Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">About</a>
+                        <a className="nav-link fs-5" href="/">About</a>
                     </li>
                     
 
