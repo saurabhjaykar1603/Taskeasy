edaaead9b3cacb19d33eb1a0562e779bb07814d7
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-27 17:16:12 +0530

    initial changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
-import Navbar from './Navbar';
 
 
 function App() {
@@ -44,8 +43,6 @@ function App() {
   }
   return (
     <>
-    <Navbar/>
-
       <div>
         <h1 className='text-center mb-4 mt-4 taskeasy-title'>Task Easy 📝</h1>
       </div>
